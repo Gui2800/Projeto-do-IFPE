1761431cb50da901e5e7217c878d6857a0a46a63
Gui2800 <44672949+Gui2800@users.noreply.github.com>
2019-02-19 23:20:39 -0300

    Create RPG.java

---
+++ RPG/RPG.java
@@ -0,0 +1,7 @@
+package rpg;
+public class RPG {
+    public static void main(String[] args) {
+        CriarPersonagem CP1 = new CriarPersonagem("Harverok", "Elfo", "Masculino", 300, "Caçador");
+        CP1.descriçao();
+    }
+}
