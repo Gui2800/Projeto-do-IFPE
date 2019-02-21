d506998f6bba7d015f786d3881810c96845e9c48
Gui2800 <44672949+Gui2800@users.noreply.github.com>
2019-02-21 09:57:06 -0300

    Create Pantano.java

---
+++ RPG/Pantano.java
@@ -0,0 +1,34 @@
+package rpg;
+public class Pantano {
+    //LEVEL 1
+    private String nomeM;
+    private double danoM;
+    private double vidaM;
+    //CHEFÃO
+    private String nomeC;
+    private double danoC;
+    private double vidaC;
+    //LEVELS
+    private int Level;
+    private String nomeL;
+
+    public double getVidaM() {
+        return vidaM;
+    }
+    public void setVidaM(double vidaM1) {
+        this.vidaM = vidaM;
+    }
+
+    public double getVidaC() {
+        return vidaC;
+    }
+
+    public void setVidaC(double vidaC) {
+        this.vidaC = vidaC;
+    }
+    
+    
+    
+    
+    
+}
