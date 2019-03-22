58489de3e914f534df107762132c3905e7d10086
Gui2800 <44672949+Gui2800@users.noreply.github.com>
2019-03-22 13:00:54 -0300

    Update login.php

--- Proj-Part1/login.php
+++ Proj-Part1/login.php
@@ -3,17 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <title>Longin</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="login.css">
 </head>
 <body>
 
 
 <?php
-    $dado = $_GET['nome'] . ';' . $_GET['sobrenome'] . ';' . $_GET['idade'] . ';' .  $_GET['e-mail']. ';' .  $_GET ['senha'] . ';' .  $_GET['matricula'] . ';' . "\n";
+    $dado = $_POST['nome'] . ';' . $_POST['sobrenome'] . ';' . $_POST['idade'] . ';' .  $_POST['e-mail']. ';' .  $_POST['senha'] . ';' . $_POST['senhaConfir'] . ';' . $_POST['matricula'] .  "\n";
     $handle = fopen('dados.csv', 'a');
     fwrite($handle, $dado);
 ?>
-
+<div>
 <div id="degrtop" class="topo">
 <img class="foto" src="logoifpe.jpg">
 
@@ -22,8 +23,8 @@
 <a href="listagem.php" class="a2">LISTAGEM</a>
 <a href="index.html" class="a3">HOME</a>
 </div>
-    <form action="login.php">
-    <div class= "input">        
+<div class= "input">        
+    <form action="login.php" method="POST">
 
         <label>Nome:</label>
         <input type="text" id="input" class="inp" name="nome" placeholder="Nome...">
@@ -42,17 +43,21 @@
         <br>
 
         <label>Senha:</label>
-        <input type="text" class="inp" name="senha" placeholder="Senha...">
+        <input type="password" class="inp" name="senha" placeholder="Senha...">
+        <br>
+
+        <label>Confirmar Senha:</label>
+        <input type="password" class="inp" name="senhaConfir" placeholder="Confirmar Senha...">
         <br>
 
         <label>Matrícula:</label>
         <input type="text" class="inp" name="matricula" placeholder="Matrícula...">
         <br>
-        <button type="submit" class="button" onclick="alert('Login efetuado com sucesso!')">Enviar</button>
+        <button type="submit" class="buuton" onclick="alert('Login efetuado com sucesso!')">Enviar</button>
    </form>
- </div> 
-
+</div> 
 <div id="debrbot" class="baixo"></div>
-   
+</div>
+<footer></footer>  
 </body>
 </html>
